f20715b40156f542a0f210c88342b3399d612e4c
SavagePastaMan <jonathanzhang929@gmail.com>
2021-01-04 13:41:10 -0500

    Add more readable prints to sudoku.py
    Created display_board function to output a readable board.

--- solver.py
+++ solver.py
@@ -1,6 +1,5 @@
 import math
 import itertools
-import scraper
 
 
 def solve(board):
--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,25 @@
 import scraper
 import solver
 import argparse
+import time
+
+def display_board(board):
+    print(*board, sep="\n", end="\n\n")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sudoku Puzzle Solver')
     parser.add_argument('-n', '--number', type=int, help="puzzle number")
     args = parser.parse_args()
+
     puzzle = scraper.get_puzzle(args.number)
-    solver.solve(puzzle)
+    print("Puzzle")
+    display_board(puzzle)
+
+    start = time.perf_counter() 
+    solved = solver.solve(puzzle)
+    end = time.perf_counter()
+
+    print("Solution")
+    display_board(solved)
+
+    print(f"Solved in {end - start:3f} seconds")
