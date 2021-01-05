d809f12f708beb295af05de4a4d21a3e2186c52d
SavagePastaMan <jonathanzhang929@gmail.com>
2021-01-04 19:15:11 -0500

    Linting

--- scraper.py
+++ scraper.py
@@ -3,21 +3,22 @@ from bs4 import BeautifulSoup
 
 from typing import Optional
 
+
 def get_puzzle(e: Optional[int]):
-    template = 'https://www.menneske.no/sudoku/eng/{}'.format
+    template = "https://www.menneske.no/sudoku/eng/{}".format
     if e is None:
-        URL = template('random.html?diff=1')
+        URL = template("random.html?diff=1")
     else:
-        URL = template(f'showpuzzle.html?number={e}')
+        URL = template(f"showpuzzle.html?number={e}")
 
     page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    rows = soup.find_all('tr', class_='grid')
+    soup = BeautifulSoup(page.content, "html.parser")
+    rows = soup.find_all("tr", class_="grid")
 
-    classes = ['bottomedge', 'bottomright', 'normal', 'rightedge']
+    classes = ["bottomedge", "bottomright", "normal", "rightedge"]
     board = []
     for row in rows:
-        boxes = row.find_all('td', class_=lambda x: x in classes)
+        boxes = row.find_all("td", class_=lambda x: x in classes)
         add = []
         for box in boxes:
             if (box.text.strip()).isdigit():
--- sudoku.py
+++ sudoku.py
@@ -3,23 +3,27 @@ import solver
 import argparse
 import time
 
+
 def display_board(board):
     print(*board, sep="\n", end="\n\n")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Sudoku Puzzle Solver')
-    parser.add_argument('-n', '--number', type=int, help="puzzle number")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Sudoku Puzzle Solver")
+    parser.add_argument("-n", "--number", type=int, help="puzzle number")
     args = parser.parse_args()
 
     puzzle = scraper.get_puzzle(args.number)
     print("Puzzle")
     display_board(puzzle)
 
-    start = time.perf_counter() 
-    solved = solver.solve(puzzle)
+    solver_ = solver.Solver(puzzle)
+
+    start = time.perf_counter()
+    solver_.solve()
     end = time.perf_counter()
 
     print("Solution")
-    display_board(solved)
+    display_board(solver_.puzzle)
 
     print(f"Solved in {end - start:3f} seconds")
